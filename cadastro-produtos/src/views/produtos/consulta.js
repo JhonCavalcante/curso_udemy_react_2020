97d66b2e6ef6a917b16891b87a0ee46ea78cc593
JhonCavalcante <fjhonatanc@gmail.com>
2021-05-15 22:39:23 -0300

    aula 31 ok, mostrando dados do storegeConsole

--- cadastro-produtos/src/views/produtos/consulta.js
+++ cadastro-produtos/src/views/produtos/consulta.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import ProdutoService from '../../app/produtoService'
 
 export default class ConsultaProdutos extends React.Component{
 
     state = {
         produtos : []
     }
+    constructor(){
+        super()
+        this.service = new ProdutoService();
+    }
+
+    componentDidMount(){
+        const produtos = this.service.obterProdutos();
+        this.setState({produtos})
+    }
+
+
 
     render(){
         return(
@@ -13,8 +25,6 @@ export default class ConsultaProdutos extends React.Component{
                     Consulta de Produto
                 </div>
                 <div className="card-body">
-
-
                     <table className="table table-houver">
                         <thead>
                             <tr>
@@ -28,9 +38,9 @@ export default class ConsultaProdutos extends React.Component{
 
                         <tbody>
                             {
-                                this.state.produtos.map( produto =>{
+                                this.state.produtos.map( (produto,index) =>{
                                     return(
-                                        <tr>
+                                        <tr key={index}>
                                             <th>{produto.nome}</th>
                                             <th>{produto.sku}</th>
                                             <th>{produto.preco}</th>
